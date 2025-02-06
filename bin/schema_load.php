084264b29a3200eb4c962d851ecce7ef519343d6
Andi Eka Nugraha <an.ekanugraha@gmail.com>
2025-02-07 00:37:08 +0700

    Update schema file path in schema_load.php

--- bin/schema_load.php
+++ bin/schema_load.php
@@ -3,7 +3,7 @@ require_once __DIR__ . "/../bootstrap.php";
 use Core\App;
 
 $db = App::get("database");
-$schemaFile = __DIR__ ."/../database/schema.sql";
+$schemaFile = __DIR__ ."/../initDatabase/schema.sql";
 $sql = file_get_contents($schemaFile);
 
 // throw new Exception("testing exception handling!");
